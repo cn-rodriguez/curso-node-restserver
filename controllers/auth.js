e6597633d178aa653fbe9e9fa84d4462ee4f7d56
Carlos Rodriguez <crodriguez.castillo.qb@gmail.com>
2022-11-22 22:53:45 -0300

    Custom data json from google identify

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,16 @@
-import { response } from 'express';
+import { json, response } from 'express';
 import { User } from '../models/user.js';
 import bcryptjs from 'bcryptjs';
 import { generarJWT } from '../helpers/generar-jwt.js';
+import { googleVerify } from '../helpers/google-verify.js';
 
 const login = async(  req, res = response ) => {
     const { email, password } = req.body;
 
     try {
 
-        // Verificar si el email existe
+        // Verificar si el email existe/* Waiting for the result of the query to the database. */
+        
         const user = await  User.findOne({ email: email });
         if ( !user ) {
             return res.status(400).json({
@@ -55,6 +57,56 @@ const login = async(  req, res = response ) => {
     // });
 }
 
+const googleSignIn = async( req, res ) => {
+    const { id_token } = req.body;
+    try {
+
+        const { email, name, picture } = await googleVerify( id_token );
+        // email exist
+        let usuario = await User.findOne({ email });
+        console.log('here')
+        
+        if ( !usuario ) {
+            const data = {
+                name,
+                email,
+                password: ':p',
+                picture,
+                google: true,
+                role: 'USER_ROLE'
+            }
+
+            usuario = new User( data )
+            await usuario.save();
+        }
+
+        // Si el usuario en db google false 
+        if ( !usuario.status ) {
+            return res.status(401).json({
+                msg: 'talk with supervisor, user blocked'  
+            });
+        }
+
+        //  Generar JWT
+        const token = await generarJWT( usuario.id )
+        console.log(token)
+
+
+        res.json({
+            usuario,
+            token,
+    })
+    }
+    catch (error) {
+        console.log(error)
+        res.status(400).json({
+            ok: false,
+            msg: 'Token cannot be verify'
+        })
+    }
+};
+
 export {
-    login
+    login,
+    googleSignIn,
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
-import { login } from '../controllers/auth.js';
+import { login, googleSignIn } from '../controllers/auth.js';
 import { validarCampos } from '../middlewares/validar-campos.js';
 
 
@@ -11,3 +11,8 @@ routerAuth.post('/login', [
     check( 'password', 'password es obligatorio').not().isEmpty(),
     validarCampos
 ] ,login);
+
+routerAuth.post('/google', [
+    check( 'id_token', 'id_token es necesario').not().isEmpty(),
+    validarCampos
+] , googleSignIn);
